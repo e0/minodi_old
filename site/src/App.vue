ea9e1852f16510faf470e71e3be937de45bc0b03
e0 <shan.huang@me.com>
2017-04-12 21:08:54 +0200

    Add site base

---
+++ site/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+body {
+  background-color: #000;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffff00;
+  margin-top: 42px;
+}
+</style>
