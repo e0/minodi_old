0ce1a4c98616d16b99cb490abebf5f68f7422dc3
e0 <shan.huang@me.com>
2017-04-15 13:57:56 +0200

    Add static site

--- site/src/App.vue
+++ site/src/App.vue
@@ -6,42 +6,51 @@
 
 <script>
 
-import { default as generateColorScheme } from './helpers/color-scheme-generator'
-
 export default {
-  name: 'app',
-  mounted () {
-    const colorScheme = generateColorScheme()
-
-    document.body.style.color = colorScheme.strong1
-    document.body.style.backgroundColor = colorScheme.pastel1
-
-    const boxes = document.getElementsByClassName('box')
-    Array.from(boxes).forEach((box) => {
-      box.style.color = colorScheme.strong2
-      box.style.backgroundColor = colorScheme.pastel2
-      box.style.border = `1px solid ${colorScheme.strong2}`
-    })
-  }
+  name: 'app'
 }
+
 </script>
 
 <style lang="scss">
+
+body {
+  background-color: #fefefe;
+}
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   max-width: 1200px;
-  margin: 42px auto;
+  margin: 42px auto 0 auto;
+
+  p {
+    font-size: 21px;
+  }
+
+  h1 {
+    font-size: 42px;
+  }
+
+  h2 {
+    font-size: 28px;
+  }
+
+  .subtitle {
+    margin-top: -21px;
+    margin-bottom: 42px;
+    font-style: italic;
+  }
 }
 
 @media all and (max-width: 1024px) {
   #app {
     max-width: 100%;
     margin: 21px;
-    p {
-      font-size: 1.25em;
+
+    h2 {
+      font-size: 21px;
     }
   }
 }
--- site/src/components/Home.vue
+++ site/src/components/Home.vue
@@ -1,34 +1,154 @@
 <template>
   <div class="home">
-    <h1>{{ msg }}</h1>
-    <div class="box">
-      <h2>Box</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h1>minodi</h1>
+    <hr>
+    <h2>a web consulting agency, from design to production</h2>
+    <h2>select what you need help with from the menu below and we will get started</h2>
+    <hr>
+    <h1>Menu</h1>
+    <p class="subtitle">you can select any number of options from any category</p>
+    <div class="section">
+      <h2>1. design</h2>
+      <span class="option">Logo</span>
+      <span class="option">UI (User interface)</span>
+      <span class="option">UX (User interaction)</span>
     </div>
+    <div class="section">
+      <h2>2. front-end development</h2>
+      <span class="option">HTML & CSS</span>
+      <span class="option">JavaScript</span>
+      <span class="option">AngularJS</span>
+      <span class="option">React</span>
+      <span class="option">Vue.js</span>
+      <span class="option">Elm</span>
+      <span class="option">Mithril</span>
+    </div>
+    <div class="section">
+      <h2>3. iOS & macOS</h2>
+      <span class="option">Objective-C</span>
+      <span class="option">Swift</span>
+      <span class="option">React Native</span>
+      <span class="option">RubyMotion</span>
+    </div>
+    <div class="section">
+      <h2>4. back-end development</h2>
+      <span class="option">Django (Python)</span>
+      <span class="option">Ruby on Rails</span>
+      <span class="option">Elixir</span>
+      <span class="option">PHP</span>
+      <span class="option">ASP.NET</span>
+    </div>
+    <div class="section">
+      <h2>5. database</h2>
+      <span class="option">PostgreSQL</span>
+      <span class="option">Microsoft SQL</span>
+      <span class="option">MySQL</span>
+      <span class="option">sqlite</span>
+      <span class="option">MongoDB</span>
+      <span class="option">RethinkDB</span>
+      <span class="option">Neo4j</span>
+    </div>
+    <div class="section">
+      <h2>7. server & automation</h2>
+      <span class="option">Ubuntu</span>
+      <span class="option">Red Hat</span>
+      <span class="option">Azure</span>
+      <span class="option">Windows Server</span>
+      <span class="option">Docker</span>
+      <span class="option">Jenkins</span>
+      <span class="option">Heroku</span>
+    </div>
+    <div class="section">
+      <h2>8. online payments</h2>
+      <span class="option">Stripe</span>
+      <span class="option">Paymill</span>
+    </div>
+    <div class="section">
+      <h2>9. social network integration</h2>
+      <span class="option">Facebook</span>
+      <span class="option">LinkedIn</span>
+      <span class="option">Twitter</span>
+    </div>
+    <hr>
+    <div>
+      <p>Happy with the selections? Fill in your contact details and click send to get started.</p>
+      <form>
+        <input type="text" placeholder="name">
+        <input type="phone" placeholder="phone">
+        <input type="email" placeholder="email">
+        <a class="button" href="#">Send</a>
+      </form>
+    </div>
+    <footer>
+      <p>minodi.com</p>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  data () {
-    return {
-      msg: 'Hello, how are you?'
-    }
-  }
+  name: 'home'
 }
 </script>
 
 <style lang="scss">
 
-.box {
-  padding: 42px;
-  border-radius: 7px;
+hr {
+  color: #000;
+  background-color: #000;
+  height: 5px;
+}
+
+.section {
+  margin-bottom: 42px;
+}
+
+.option, form input, form a {
+  display: inline-block;
+  width: calc(25% - 18px);
+  cursor: pointer;
+  margin: 7px;
+  padding: 7px;
+  text-align: center;
+  border: 2px solid;
+  box-sizing: border-box;
+  border-radius: 0;
+}
+
+.option {
+  font-size: 28px;
+
+  &:hover {
+    background-color: #acdfac;
+  }
+}
+
+form {
+  input {
+    font-size: 21px;
+    line-height: 28px;
+  }
+
+  a {
+    color: #fefefe;
+    background-color: #000;
+    font-size: 28px;
+    line-height: 32px;
+    text-decoration: none;
+  }
+}
+
+footer {
+  margin-top: 63px;
+  padding: 7px;
+  margin-bottom: 0;
+  text-align: center;
 }
 
 @media all and (max-width: 1024px) {
-  .box {
-    padding: 21px;
+  .option, form input, form a {
+    display: block;
+    width: 100%;
   }
 }
 
